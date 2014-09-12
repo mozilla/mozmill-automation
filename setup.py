0b547aee5e3cd16ff8722be76393a9aa56e7d881
Henrik Skupin <mail@hskupin.info>
2014-09-12 14:19:39 +0200

    Upgrade mozversion for handling new signed builds on OS X (#166)

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ deps = ['mercurial == 2.6.2',
         'mozfile == 1.1',
         'mozinstall == 1.9',
         'mozmill == 2.0.6',
-        'mozversion >= 0.4'
+        'mozversion >= 0.7'
         ]
 
 setup(name=NAME,
