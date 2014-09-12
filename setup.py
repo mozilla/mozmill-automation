427e4f9ab6a45f8b9b2405d39bedf902d4b4d5ea
Henrik Skupin <mail@hskupin.info>
2014-09-12 15:23:18 +0200

    Release version 2.0.7 (#170)

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ except IOError:
     description = None
 
 NAME = 'mozmill-automation'
-VERSION = '2.0.6.2'
+VERSION = '2.0.7'
 
 deps = ['mercurial == 2.6.2',
         'mozdownload == 1.11.1',
         'mozfile == 1.1',
         'mozinstall == 1.9',
-        'mozmill == 2.0.6',
+        'mozmill == 2.0.7',
         'mozversion >= 0.7'
         ]
 
